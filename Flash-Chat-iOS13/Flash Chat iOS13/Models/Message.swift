c728e97f458e1cc27fd5bb72c3c8826d63e5dd94
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-20 11:44:49 +0800

    Add files via upload

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Guo-Feng Luo on 2024/5/19.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
